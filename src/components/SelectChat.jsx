7163f04516681333d2cb4753cfcfdb2697893465
luiggy <luiggylml@gmail.com>
2022-11-21 10:56:10 -0500

    change img

--- src/components/SelectChat.jsx
+++ src/components/SelectChat.jsx
@@ -4,7 +4,10 @@ const SelectChat = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div>
-        <img src="../../assets/seleccion-chat.svg" alt="" />
+        <img
+          src="https://res.cloudinary.com/dhtyfvv9i/image/upload/v1669046133/seleccion-chat_bpkwgc.svg"
+          alt=""
+        />
         <h1 className="text-2xl text-center">Seleccione un Chat </h1>
       </div>
     </div>
